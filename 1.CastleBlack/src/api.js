3e685ca33d94636a12e66956e16024159f398b81
TDG <orione_90@hotmail.com>
2019-11-06 20:27:08 +0100

    CastleBlack: destroy objects, update postman collection

--- 1.CastleBlack/src/api.js
+++ 1.CastleBlack/src/api.js
@@ -37,28 +37,25 @@ api.get("/players/:id", function(req, res) {
     const player = players.find(p => p.id === parseInt(req.params.id));
 
     // Validation error 400 bad request
-    if (!player) {
-        res.status(404).json("The player with the given ID was not found!");
-    } else {
-        // Display result
-        res.json(player);
-    }
+    if (!player) return res.status(404).json("The player with the given ID was not found!");
+
+    // Display result
+    res.json(player);
 });
 
 // ADD A PLAYER
 api.post("/players", function(req, res) {
 
     // Validation error 400 bad request
-    if (!req.body.name || req.body.name.length < 3) {
-        res.status(400).json("Name is required and should be minimum 3 characters");
-        return;
-    } else if (!req.body.age || (req.body.age < 18 || req.body.age > 60)) {
-        res.status(400).json("Age is required and should be between 18 and 60");
-        return;
-    } else if (!req.body.bag || (req.body.bag[0] < 0 || req.body.bag[0] > 5)) {
-        res.status(400).json("Bag is required and should be between 1 and 5");
-        return;
-    }
+    if (!req.body.name || req.body.name.length < 3)
+        return res.status(400).json("Name is required and should be minimum 3 characters");
+
+    else if (!req.body.age || (req.body.age < 18 || req.body.age > 60))
+        return res.status(400).json("Age is required and should be between 18 and 60");
+
+    else if (!req.body.bag || (req.body.bag[0] < 0 || req.body.bag[0] > 5))
+        return res.status(400).json("Bag is required and should be between 1 and 5");
+
 
     // Creat new player
     const player = {
@@ -73,6 +70,22 @@ api.post("/players", function(req, res) {
     players.push(player);
     res.json(player);
 });
+
+// DELETE A PLAYER
+api.delete("/players/:id", function(req, res) {
+    //Look if the copurse exist
+    const player = players.find(p => p.id === parseInt(req.params.id));
+    // Validation error 404 if object doesnt exist
+    if (!player) return res.status(404).json("The player with the given ID was not found!");
+
+    // Destroy player
+    const index = players.indexOf(player);
+    objects.splice(index, 1);
+
+    // Return the same player
+    res.json(player);
+});
+
 
 // ENDPOINT: OBJECTS
 // LIST ALL OBJECTS
@@ -128,6 +141,22 @@ api.post("/objects", function(req, res) {
     objects.push(object);
     res.json(object);
 });
+
+// DELETE AN OBJECT
+api.delete("/objects/:id", function(req, res) {
+    //Look if the copurse exist
+    const object = objects.find(o => o.id === parseInt(req.params.id));
+    // Validation error 404 if object doesnt exist
+    if (!object) return res.status(404).json("The object with the given ID was not found!");
+
+    // Destroy object
+    const index = objects.indexOf(object);
+    objects.splice(index, 1);
+
+    // Return the same object
+    res.json(object);
+});
+
 
 
 module.exports = api
